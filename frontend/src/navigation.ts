93da1ba0c90f05f97e8e0735fada85f96cf126f0
Facundo <fakumax@gmail.com>
2026-04-21 10:51:46 -0300

    feature: Ajustes en portal front

--- frontend/src/navigation.ts
+++ frontend/src/navigation.ts
@@ -1,4 +1,4 @@
-import { getPermalink, getBlogPermalink, getAsset } from './utils/permalinks';
+import { getPermalink, getBlogPermalink } from './utils/permalinks';
 
 export const headerData = {
   links: [
@@ -7,23 +7,23 @@ export const headerData = {
       href: getPermalink('/'),
     },
     {
-      text: 'Nosotros',
-      href: getPermalink('/about'),
+      text: 'Quiénes somos',
+      href: getPermalink('/quienes-somos'),
     },
     {
-      text: 'Noticias (WP)',
+      text: 'Qué hacemos',
+      href: getPermalink('/que-hacemos'),
+    },
+    {
+      text: 'Noticias',
       href: getPermalink('/noticias'),
     },
     {
-      text: 'Blog',
-      href: getBlogPermalink(),
-    },
-    {
       text: 'Contacto',
-      href: getPermalink('/contact'),
+      href: getPermalink('/contacto'),
     },
   ],
-  actions: [{ text: 'AYUDA URGENTE', href: 'tel:911', class: 'bg-red-600 hover:bg-red-700' }],
+  actions: [{ text: 'AYUDA URGENTE · 135', href: 'tel:135', class: 'bg-red-600 hover:bg-red-700' }],
 };
 
 export const footerData = {
@@ -31,27 +31,36 @@ export const footerData = {
     {
       title: 'Asociación',
       links: [
-        { text: 'Nosotros', href: getPermalink('/about') },
+        { text: 'Quiénes somos', href: getPermalink('/quienes-somos') },
+        { text: 'Qué hacemos', href: getPermalink('/que-hacemos') },
         { text: 'Noticias', href: getPermalink('/noticias') },
-        { text: 'Contacto', href: getPermalink('/contact') },
+        { text: 'Contacto', href: getPermalink('/contacto') },
       ],
     },
     {
       title: 'Nuestros Pilares',
       links: [
-        { text: 'Prevención', href: '#' },
-        { text: 'Orientación', href: '#' },
-        { text: 'Postvención', href: '#' },
-        { text: 'Grupos GARE', href: '#' },
+        { text: 'Prevención', href: '/que-hacemos#prevencion' },
+        { text: 'Orientación', href: '/que-hacemos#orientacion' },
+        { text: 'Postvención', href: '/que-hacemos#postvencion' },
+        { text: 'Grupos GARE', href: '/que-hacemos#gare' },
       ],
     },
     {
       title: 'Contacto',
       links: [
-        { text: 'Av. Sáenz Peña 2547, Posadas', href: '#' },
+        { text: 'Posadas, Misiones, Argentina', href: '#' },
         { text: 'WhatsApp: 3764-385252', href: 'https://wa.me/5493764385252' },
         { text: 'Emergencias: 911', href: 'tel:911' },
         { text: 'Línea 135 (Prevención)', href: 'tel:135' },
+      ],
+    },
+    {
+      title: 'Datos Legales',
+      links: [
+        { text: 'Personería Jurídica Nº A-2839', href: '#' },
+        { text: 'Entidad de Bien Público N°409', href: '#' },
+        { text: 'CUIT: 30-70972472-6', href: '#' },
       ],
     },
   ],
@@ -61,8 +70,9 @@ export const footerData = {
   ],
   socialLinks: [
     { ariaLabel: 'WhatsApp', icon: 'tabler:brand-whatsapp', href: 'https://wa.me/5493764385252' },
-    { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: '#' },
-    { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: '#' },
+    { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: 'https://www.facebook.com/defenderlavida' },
+    { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: 'https://www.instagram.com/defenderlavida' },
+    { ariaLabel: 'Email', icon: 'tabler:mail', href: 'mailto:defenderlavida@gmail.com' },
   ],
   footNote: `
     © Hablemos de Defender la Vida · Posadas, Misiones, Argentina · Todos los derechos reservados.
